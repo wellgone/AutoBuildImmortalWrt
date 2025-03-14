4068ee6daafcf30d3704101e270e30300b0db8c6
well <well_gone@163.com>
2025-03-14 18:37:28 +0800

    修复

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -54,7 +54,6 @@ PACKAGES="$PACKAGES luci-app-ddns"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-app-advanced-reboot"
 PACKAGES="$PACKAGES luci-i18n-advanced-reboot-zh-cn"
-PACKAGES="$PACKAGES luci-app-qos"
 	
  
 
