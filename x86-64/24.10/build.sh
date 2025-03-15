71f5dea91421eb69e9e856378e44f8048bf9ab97
well <well_gone@163.com>
2025-03-15 14:33:58 +0800

    修复

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -48,8 +48,6 @@ PACKAGES="$PACKAGES luci-app-wechatpush"
 PACKAGES="$PACKAGES luci-app-wechatpush-zh-cn"
 PACKAGES="$PACKAGES luci-app-ddns"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
-PACKAGES="$PACKAGES luci-app-advanced-reboot"
-PACKAGES="$PACKAGES luci-i18n-advanced-reboot-zh-cn"
 	
  
 
