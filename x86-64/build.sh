260712793becc8cb95d4e6f2339cd4028b9431ce
well <well_gone@163.com>
2025-03-15 19:26:18 +0800

    修复

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -46,6 +46,9 @@ PACKAGES="$PACKAGES luci-app-ddns"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-app-advanced-reboot"
 PACKAGES="$PACKAGES luci-i18n-advanced-reboot-zh-cn"
+PACKAGES="$PACKAGES luci-app-wechatpush"
+PACKAGES="$PACKAGES luci-i18n-wechatpush-zh-cn"
+
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
